0bff8e1559c00d8792e76792dcd71af5e1e5d5dd
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-09-24 18:21:15 +0530

    Firebase Basic Config

--- firechat/src/App.js
+++ firechat/src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import './App.css';
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+firebase.initializeApp({
+
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
